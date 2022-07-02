fec6163544ba01356a1ea7be12331d99e381b042
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-07-02 21:21:39 +0530

    all img effect applied

--- index.js
+++ index.js
@@ -58,10 +58,62 @@ lov_6.onmouseout = ()=>{
     img_text_6.style.display = "none"
 }
 
-let main_div1 =document.getElementById("main_div1")
-let icon_per =document.getElementById("icon_per")
-let person_details =document.getElementById("person_details")
+let main_div1 =document.querySelector("#main_div1")
+let icon_per =document.querySelector("#icon_per")
+let person_details =document.querySelector("#person_details")
 
 main_div1.onmouseover =()=>{
-    
+    icon_per.style.display="block"
+    person_details.style.backgroundColor ="rgb(58,69,77)"
+    person_details.style.color ="white"
+}
+main_div1.onmouseout =()=>{
+    icon_per.style.display="none"
+    person_details.style.backgroundColor ="#c8cbcf"
+    person_details.style.color ="black"
+}
+//for pic 2
+let main_div2 =document.querySelector("#main_div2")
+let icon_per2 =document.querySelector("#icon_per2")
+let person_details2 =document.querySelector("#person_details2")
+
+main_div2.onmouseover =()=>{
+    icon_per2.style.display="block"
+    person_details2.style.backgroundColor ="rgb(58,69,77)"
+    person_details2.style.color ="white"
+}
+main_div2.onmouseout =()=>{
+    icon_per2.style.display="none"
+    person_details2.style.backgroundColor ="#c8cbcf"
+    person_details2.style.color ="black"
+}
+//for pic 3
+let main_div3 =document.querySelector("#main_div3")
+let icon_per3 =document.querySelector("#icon_per3")
+let person_details3 =document.querySelector("#person_details3")
+
+main_div3.onmouseover =()=>{
+    icon_per3.style.display="block"
+    person_details3.style.backgroundColor ="rgb(58,69,77)"
+    person_details3.style.color ="white"
+}
+main_div3.onmouseout =()=>{
+    icon_per3.style.display="none"
+    person_details3.style.backgroundColor ="#c8cbcf"
+    person_details3.style.color ="black"
+}
+//for pic 4
+let main_div4 =document.querySelector("#main_div4")
+let icon_per4 =document.querySelector("#icon_per4")
+let person_details4 =document.querySelector("#person_details4")
+
+main_div4.onmouseover =()=>{
+    icon_per4.style.display="block"
+    person_details4.style.backgroundColor ="rgb(58,69,77)"
+    person_details4.style.color ="white"
+}
+main_div4.onmouseout =()=>{
+    icon_per4.style.display="none"
+    person_details4.style.backgroundColor ="#c8cbcf"
+    person_details4.style.color ="black"
 }
