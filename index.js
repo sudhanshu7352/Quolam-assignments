914b7f6ae2aca0fc416e6321cdd16663d8297f2a
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-07-02 20:00:58 +0530

    image updated and effect added

---
+++ index.js
@@ -0,0 +1,67 @@
+
+let lov_1 =document.querySelector(".lov_1")
+let img_text =document.querySelector(".img_text")
+
+lov_1.onmouseover = ()=>{
+    img_text.style.display = "block"
+}
+lov_1.onmouseout = ()=>{
+    img_text.style.display = "none"
+}
+
+let lov_2 =document.querySelector(".lov_2")
+let img_text_2 =document.querySelector(".img_text_2")
+
+lov_2.onmouseover = ()=>{
+    img_text_2.style.display = "block"
+}
+lov_2.onmouseout = ()=>{
+    img_text_2.style.display = "none"
+}
+
+let lov_3 =document.querySelector(".lov_3")
+let img_text_3 =document.querySelector(".img_text_3")
+
+lov_3.onmouseover = ()=>{
+    img_text_3.style.display = "block"
+}
+lov_3.onmouseout = ()=>{
+    img_text_3.style.display = "none"
+}
+
+let lov_4 =document.querySelector(".lov_4")
+let img_text_4 =document.querySelector(".img_text_4")
+
+lov_4.onmouseover = ()=>{
+    img_text_4.style.display = "block"
+}
+lov_4.onmouseout = ()=>{
+    img_text_4.style.display = "none"
+}
+
+let lov_5 =document.querySelector(".lov_5")
+let img_text_5 =document.querySelector(".img_text_5")
+
+lov_5.onmouseover = ()=>{
+    img_text_5.style.display = "block"
+}
+lov_5.onmouseout = ()=>{
+    img_text_5.style.display = "none"
+}
+let lov_6 =document.querySelector(".lov_6")
+let img_text_6 =document.querySelector(".img_text_6")
+
+lov_6.onmouseover = ()=>{
+    img_text_6.style.display = "block"
+}
+lov_6.onmouseout = ()=>{
+    img_text_6.style.display = "none"
+}
+
+let main_div1 =document.getElementById("main_div1")
+let icon_per =document.getElementById("icon_per")
+let person_details =document.getElementById("person_details")
+
+main_div1.onmouseover =()=>{
+    
+}
